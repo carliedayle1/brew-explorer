2e3092ef7ece79ea245cfa7f3814c5baf66cb351
Erick Dayle Loon <eloon@timelessmedical.com>
2023-06-13 17:36:57 +0800

    Removed context and lib

--- src/app/components/LoaderSpinner.js
+++ src/app/components/LoaderSpinner.js
@@ -2,7 +2,7 @@ import { InfinitySpin } from "react-loader-spinner";
 
 const LoaderSpinner = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center min-h-screen">
       <InfinitySpin width="200" color="#fbbf24" />
     </div>
   );
